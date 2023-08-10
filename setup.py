5e698e7f51ba0a7c5dca612a6c8d9dace0d29bb3
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2023-08-10 15:04:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='biospecml',
     version='0.1dev',
-    packages=['preprocessing','models'],
+    package_dir = {"": "src"},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
