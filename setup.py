f8153584b92ae1967fe3fe5733cc51fcc3f3784a
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2023-08-10 15:39:42 +0100

    redo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='biospecml',
+    version='0.1dev',
+    package_dir = {"": "src"},
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
