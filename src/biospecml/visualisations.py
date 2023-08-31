1ed8e17c6ffc74e1fb9d269cac7f98fc881dacbc
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2023-08-31 17:43:55 +0100

    Update visualisations.py

--- src/biospecml/visualisations.py
+++ src/biospecml/visualisations.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import os
+import matplotlib.image as mpimg
 
 def plot_rxc(nrows=1, ncols=2, dpi=120, figsize=(9,3), imgs=[], titles=[],
              title_size=10, cmap='binary', show=True, fname=None):
@@ -34,4 +36,52 @@ def plot_rxc(nrows=1, ncols=2, dpi=120, figsize=(9,3), imgs=[], titles=[],
         plt.close()
     elif show==True:
         plt.show(block=False);
+        plt.close()
+      
+
+def plot_images_from_folder(
+    folder_path, rows=1, cols=1, img_format='.png', figsize=(10, 10),
+    title_size=10, cmap='binary', cmap_reverse=False, fname=None,
+    save_dpi=200, show_plot=True):
+    """
+    Plot images from a folder in a single figure.
+
+    Args:
+        folder_path (str): Path to the folder containing images.
+        rows (int): Number of rows in the subplot grid.
+        cols (int): Number of columns in the subplot grid.
+        figsize (tuple): Figure size (width, height) in inches.
+    """
+    fig, axes = plt.subplots(rows, cols, figsize=figsize)
+    axes = axes.flatten()
+
+    # === get the images ===
+    # image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+    image_files = [f for f in os.listdir(folder_path) if f.endswith((img_format))]
+
+    # === plot each images ===
+    for i, ax in enumerate(axes):
+        if i < len(image_files):
+            img_path = os.path.join(folder_path, image_files[i])
+            img = mpimg.imread(img_path)
+            if cmap_reverse==False:
+                ax.imshow(img, cmap=plt.colormaps.get_cmap(cmap))
+            elif cmap_reverse==True:
+                ax.imshow(img, cmap=plt.colormaps.get_cmap(cmap).reversed())
+            ax.set_title(image_files[i], fontsize=title_size)
+            ax.axis('off')
+        else:
+            ax.axis('off')
+    plt.tight_layout()
+
+    # === show condition ===
+    if show_plot==True:
+        plt.show()
+        # save condition
+        if fname!=None:
+            plt.savefig(fname, dpi=save_dpi)
+    elif show_plot==False:
+        # save condition
+        if fname!=None:
+            plt.savefig(fname, dpi=save_dpi)
         plt.close()
