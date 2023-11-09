a9ef751422fab4894fe1bf7cbb81b139d5ac3643
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2023-11-09 15:43:20 +0000

    new calc_DatasetMeanStd

--- src/biospecml/ml/ml.py
+++ src/biospecml/ml/ml.py
@@ -147,7 +147,9 @@ def rotate_and_flip(images=[], pair_mode=False, rotate=True, rotate_method='nump
 
     return augmented_images
 
-def calc_DatasetMeanStd(loader, channels, data_type=torch.float64, data_position=None):
+def calc_DatasetMeanStd(loader, channels, data_type=torch.float32, 
+                        data_position=None, replace_zeros=False,
+                        replace_value=1.4013e-45):
     """
     Calculate mean and std of the channels in each image in the data loader,
     and average them, expect approximation of the values due to averaging because
@@ -156,17 +158,21 @@ def calc_DatasetMeanStd(loader, channels, data_type=torch.float64, data_position
     Args:
         loader(torch.utils.data.DataLoader): accept data loader that output single or double data pair
         channels(int): number of channels of the image
+        data_type(torch.dtype): torch data type to hold the mean/std value
         data_position(int): position of data output from data loader (0 or 1)
+        replace_zeros(bool): condition to replace zeros in mean/std value
+        replace_value(float): the default is the minimum value in float32
     Returns:
         mean(float): mean of all data in data loader
         std(float): std of all data in data loader
     """
 
-    # __declare variables__
+    #--- declare variables ---
     total_mean = torch.zeros(channels, dtype=data_type)
     total_std = torch.zeros(channels, dtype=data_type)
     count = 0
 
+    #--- condition of the data loader outputs ---
     """ in the case of data loader that output two set of data (i.e: image, label)
         the following condition allow user to select where is the position (0 or 1) of the
         data for calculation (image or label).
@@ -174,24 +180,81 @@ def calc_DatasetMeanStd(loader, channels, data_type=torch.float64, data_position
     for batch in loader:
         if len(batch) == 1:
             # the data loader only output 1 data
-            # print('Loader output 1 data')
             img = batch
         elif len(batch) == 2:
             # the data loader output 2 data
-            # print('Loader output 2 data, getting data_position.')
             img = batch[data_position]
         else:
             # the data loader output >= 2 data
             raise Exception('Only support either 0 or 1 data position.')
 
-        # __calculations__
+        #--- calculate mean/std ---
         mean = img.view(channels, -1).mean(dim=1)
         std = img.view(channels, -1).std(dim=1)
         total_mean += mean; total_std += std; count += 1
 
-    # calculate averages  
+    #--- averaging mean/std ---
     ave_mean, ave_std = total_mean/count, total_std/count
+
+    #--- replace zero condition ---
+    """ important to replace zeros if data will be use for dataset normalisation 
+        because division error will occur if there's zeros values in std/mean.
+    """
+    if replace_zeros==True:
+        data = [ave_mean, ave_std] # compile averages into a list
+        for ave in data: # loop through averages
+            zero_indices = (ave==0) # find any zeros in the mean/std
+            if (zero_indices==True).any()==True:
+                print('Replacing zero(s)..')
+                ave[zero_indices] = replace_value # replacement
+
     return ave_mean, ave_std
+
+# def calc_DatasetMeanStd(loader, channels, data_type=torch.float64, data_position=None):
+#     """
+#     Calculate mean and std of the channels in each image in the data loader,
+#     and average them, expect approximation of the values due to averaging because
+#     stacking all ftir tensors tend to be huge to load in memory.
+
+#     Args:
+#         loader(torch.utils.data.DataLoader): accept data loader that output single or double data pair
+#         channels(int): number of channels of the image
+#         data_position(int): position of data output from data loader (0 or 1)
+#     Returns:
+#         mean(float): mean of all data in data loader
+#         std(float): std of all data in data loader
+#     """
+
+#     # __declare variables__
+#     total_mean = torch.zeros(channels, dtype=data_type)
+#     total_std = torch.zeros(channels, dtype=data_type)
+#     count = 0
+
+#     """ in the case of data loader that output two set of data (i.e: image, label)
+#         the following condition allow user to select where is the position (0 or 1) of the
+#         data for calculation (image or label).
+#     """
+#     for batch in loader:
+#         if len(batch) == 1:
+#             # the data loader only output 1 data
+#             # print('Loader output 1 data')
+#             img = batch
+#         elif len(batch) == 2:
+#             # the data loader output 2 data
+#             # print('Loader output 2 data, getting data_position.')
+#             img = batch[data_position]
+#         else:
+#             # the data loader output >= 2 data
+#             raise Exception('Only support either 0 or 1 data position.')
+
+#         # __calculations__
+#         mean = img.view(channels, -1).mean(dim=1)
+#         std = img.view(channels, -1).std(dim=1)
+#         total_mean += mean; total_std += std; count += 1
+
+#     # calculate averages  
+#     ave_mean, ave_std = total_mean/count, total_std/count
+#     return ave_mean, ave_std
 
 
 def save_tensor_to_file(tensor, fname=None, delimiter='\t'):
