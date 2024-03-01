49fba6686c75efd9ac847f61f02377e91b1de76b
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2024-03-01 15:04:31 +0000

    metrics.py and stuffs

---
+++ src/biospecml/ml/metrics.py
@@ -0,0 +1,87 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from sklearn.metrics import f1_score, accuracy_score
+import numpy as np
+
+# --------------- helper functions ---------------
+
+def calc_metric_prediction(inputs, outputs, metrics_list=['accuracy', 'f1'], f1_average='macro'):
+    """
+    Evaluate the performance of a model for classification tasks using various metrics.
+
+    Args:
+    - inputs (torch.Tensor): Original input labels (ground truth).
+    - outputs (torch.Tensor): Predicted output labels.
+    - metric (str): Metric to compute. Options: 'accuracy', 'f1', 'all' (default).
+    - threshold (float): Threshold for binary classification (default: 0.5).
+
+    Returns:
+    - result (dict): Computed metric value or a dictionary containing different evaluation metrics.
+    """
+    metrics = {}
+
+    if not isinstance(inputs, np.ndarray) or not isinstance(outputs, np.ndarray):
+        raise Exception('Inputs/outputs must be numpy array for predictions.')
+
+    for metric in metrics_list:
+
+        if metric not in ['accuracy', 'f1']:
+            raise ValueError(f"Invalid metric. Choose from 'accuracy' or/and 'f1'.")
+
+        if metric == 'f1':
+            f1 = f1_score(inputs, outputs, average=f1_average)
+            metrics['f1'] = f1
+
+        if metric == 'accuracy':
+            accuracy = accuracy_score(inputs, outputs)
+            metrics['accuracy'] = accuracy
+
+    return metrics
+
+
+def calc_metric_reconstruction(inputs, outputs, metrics_list=['SSIM']):
+    """
+    Evaluate the performance of a recontructive model using various metrics.
+
+    Args:
+    - inputs (np.ndarray): Original input images.
+    - outputs (np.ndarray): Reconstructed output images.
+    - metric (str): Metric to compute. Options: 'MSE', 'BCE', 'MAE', 'SSIM', 'PSNR'.
+
+    Returns:
+    - metrics (dict): Computed metric value or a dictionary containing different evaluation metrics.
+
+    Example:
+    >>> arr1, arr2 = np.random.rand(1, 3, 16, 16), np.random.rand(1, 3, 16, 16)
+    >>> metrics = calc_metric_reconstruction(arr1, arr2, metrics_list=['SSIM'])
+
+    """
+    metrics = {}
+    metrics_list_ref = ['MSE', 'MAE', 'SSIM', 'PSNR']
+
+    for metric in metrics_list:
+
+        # check metrics
+        if metric not in metrics_list_ref:
+            raise ValueError(f"Invalid metric. Choose among {metrics_list_ref}")
+
+        # Mean Squared Error (MSE)
+        if metric=='MSE':
+            metrics['MSE'] = mean_squared_error(inputs, outputs)
+
+        # Mean Absolute Error (MAE)
+        if metric=='MAE':
+            metrics['MAE'] = mean_absolute_error(inputs, outputs)
+
+        # Structural Similarity Index (SSIM)
+        if metric=='SSIM':
+            multichannel = False if len(inputs.shape) == 2 else True
+            ssim_value = ssim(inputs, outputs, multichannel=multichannel)
+            metrics['SSIM'] = np.mean(ssim_value)
+
+        # Peak Signal-to-Noise Ratio (PSNR)
+        if metric=='PSNR':
+            metrics['PSNR'] = psnr(inputs, outputs)
+
+    return metrics
--- src/biospecml/training_loops/train_loop.py
+++ src/biospecml/training_loops/train_loop.py
@@ -1,7 +1,8 @@
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from skimage.metrics import structural_similarity as ssim
-from skimage.metrics import peak_signal_noise_ratio as psnr
-from sklearn.metrics import f1_score, accuracy_score
+# from sklearn.metrics import mean_squared_error, mean_absolute_error
+# from skimage.metrics import structural_similarity as ssim
+# from skimage.metrics import peak_signal_noise_ratio as psnr
+# from sklearn.metrics import f1_score, accuracy_score
+from ml.metrics import calc_metric_prediction, calc_metric_reconstruction
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -11,85 +12,85 @@ import os
 
 # --------------- helper functions ---------------
 
-def calc_metric_prediction(inputs, outputs, metrics_list=['accuracy', 'f1'], f1_average='macro'):
-    """
-    Evaluate the performance of a model for classification tasks using various metrics.
+# def calc_metric_prediction(inputs, outputs, metrics_list=['accuracy', 'f1'], f1_average='macro'):
+#     """
+#     Evaluate the performance of a model for classification tasks using various metrics.
 
-    Args:
-    - inputs (torch.Tensor): Original input labels (ground truth).
-    - outputs (torch.Tensor): Predicted output labels.
-    - metric (str): Metric to compute. Options: 'accuracy', 'f1', 'all' (default).
-    - threshold (float): Threshold for binary classification (default: 0.5).
+#     Args:
+#     - inputs (torch.Tensor): Original input labels (ground truth).
+#     - outputs (torch.Tensor): Predicted output labels.
+#     - metric (str): Metric to compute. Options: 'accuracy', 'f1', 'all' (default).
+#     - threshold (float): Threshold for binary classification (default: 0.5).
 
-    Returns:
-    - result (dict): Computed metric value or a dictionary containing different evaluation metrics.
-    """
-    metrics = {}
+#     Returns:
+#     - result (dict): Computed metric value or a dictionary containing different evaluation metrics.
+#     """
+#     metrics = {}
 
-    if not isinstance(inputs, np.ndarray) or not isinstance(outputs, np.ndarray):
-        raise Exception('Inputs/outputs must be numpy array for predictions.')
+#     if not isinstance(inputs, np.ndarray) or not isinstance(outputs, np.ndarray):
+#         raise Exception('Inputs/outputs must be numpy array for predictions.')
 
-    for metric in metrics_list:
+#     for metric in metrics_list:
 
-        if metric not in ['accuracy', 'f1']:
-            raise ValueError(f"Invalid metric. Choose from 'accuracy' or/and 'f1'.")
+#         if metric not in ['accuracy', 'f1']:
+#             raise ValueError(f"Invalid metric. Choose from 'accuracy' or/and 'f1'.")
 
-        if metric == 'f1':
-            f1 = f1_score(inputs, outputs, average=f1_average)
-            metrics['f1'] = f1
+#         if metric == 'f1':
+#             f1 = f1_score(inputs, outputs, average=f1_average)
+#             metrics['f1'] = f1
 
-        if metric == 'accuracy':
-            accuracy = accuracy_score(inputs, outputs)
-            metrics['accuracy'] = accuracy
+#         if metric == 'accuracy':
+#             accuracy = accuracy_score(inputs, outputs)
+#             metrics['accuracy'] = accuracy
 
-    return metrics
+#     return metrics
 
 
-def calc_metric_reconstruction(inputs, outputs, metrics_list=['SSIM']):
-    """
-    Evaluate the performance of a recontructive model using various metrics.
+# def calc_metric_reconstruction(inputs, outputs, metrics_list=['SSIM']):
+#     """
+#     Evaluate the performance of a recontructive model using various metrics.
 
-    Args:
-    - inputs (np.ndarray): Original input images.
-    - outputs (np.ndarray): Reconstructed output images.
-    - metric (str): Metric to compute. Options: 'MSE', 'BCE', 'MAE', 'SSIM', 'PSNR'.
+#     Args:
+#     - inputs (np.ndarray): Original input images.
+#     - outputs (np.ndarray): Reconstructed output images.
+#     - metric (str): Metric to compute. Options: 'MSE', 'BCE', 'MAE', 'SSIM', 'PSNR'.
 
-    Returns:
-    - metrics (dict): Computed metric value or a dictionary containing different evaluation metrics.
+#     Returns:
+#     - metrics (dict): Computed metric value or a dictionary containing different evaluation metrics.
 
-    Example:
-    >>> arr1, arr2 = np.random.rand(1, 3, 16, 16), np.random.rand(1, 3, 16, 16)
-    >>> metrics = calc_metric_reconstruction(arr1, arr2, metrics_list=['SSIM'])
+#     Example:
+#     >>> arr1, arr2 = np.random.rand(1, 3, 16, 16), np.random.rand(1, 3, 16, 16)
+#     >>> metrics = calc_metric_reconstruction(arr1, arr2, metrics_list=['SSIM'])
 
-    """
-    metrics = {}
-    metrics_list_ref = ['MSE', 'MAE', 'SSIM', 'PSNR']
+#     """
+#     metrics = {}
+#     metrics_list_ref = ['MSE', 'MAE', 'SSIM', 'PSNR']
 
-    for metric in metrics_list:
+#     for metric in metrics_list:
 
-        # check metrics
-        if metric not in metrics_list_ref:
-            raise ValueError(f"Invalid metric. Choose among {metrics_list_ref}")
+#         # check metrics
+#         if metric not in metrics_list_ref:
+#             raise ValueError(f"Invalid metric. Choose among {metrics_list_ref}")
 
-        # Mean Squared Error (MSE)
-        if metric=='MSE':
-            metrics['MSE'] = mean_squared_error(inputs, outputs)
+#         # Mean Squared Error (MSE)
+#         if metric=='MSE':
+#             metrics['MSE'] = mean_squared_error(inputs, outputs)
 
-        # Mean Absolute Error (MAE)
-        if metric=='MAE':
-            metrics['MAE'] = mean_absolute_error(inputs, outputs)
+#         # Mean Absolute Error (MAE)
+#         if metric=='MAE':
+#             metrics['MAE'] = mean_absolute_error(inputs, outputs)
 
-        # Structural Similarity Index (SSIM)
-        if metric=='SSIM':
-            multichannel = False if len(inputs.shape) == 2 else True
-            ssim_value = ssim(inputs, outputs, multichannel=multichannel)
-            metrics['SSIM'] = np.mean(ssim_value)
+#         # Structural Similarity Index (SSIM)
+#         if metric=='SSIM':
+#             multichannel = False if len(inputs.shape) == 2 else True
+#             ssim_value = ssim(inputs, outputs, multichannel=multichannel)
+#             metrics['SSIM'] = np.mean(ssim_value)
 
-        # Peak Signal-to-Noise Ratio (PSNR)
-        if metric=='PSNR':
-            metrics['PSNR'] = psnr(inputs, outputs)
+#         # Peak Signal-to-Noise Ratio (PSNR)
+#         if metric=='PSNR':
+#             metrics['PSNR'] = psnr(inputs, outputs)
 
-    return metrics
+#     return metrics
 
 
 # --------------- running loop ---------------
@@ -207,3 +208,101 @@ def train_model(model, data_loader, device, num_epochs, criterion, optimizer,
                 json.dump(metrics, json_file, indent=4)
 
     return model, metrics
+
+
+def train_val_loop(model, device, num_epochs, criterion, optimizer,
+                   train_loader, test_loader=None, trained_num_epochs:int=None,
+                   running_type:str='predictions',
+                   f1_average:str='macro', metrics_list:list=['f1', 'accuracy'],
+                   savedir:str=None, epoch_save_checkpoints:list=[],
+                   save_model:bool=True,
+                   ):
+    """
+    Example of use:
+    >>> learning_rate , weight_decay = 0.001, 0
+    >>> model, main_metrics = train_val_loop(
+    >>>     model = model,
+    >>>     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    >>>     num_epochs = 3,
+    >>>     criterion = nn.CrossEntropyLoss(),
+    >>>     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay),
+    >>>     train_loader = train_loader,
+    >>>     test_loader = test_loader,
+    >>>     running_type = 'prediction',
+    >>>     f1_average = 'macro',
+    >>>     metrics_list = ['f1', 'accuracy'],
+    >>>     savedir = os.getcwd(),
+    >>>     epoch_save_checkpoints = [2,3]
+    >>> )
+    """
+    start_epoch = trained_num_epochs+1 if isinstance(trained_num_epochs, int) else 1
+    main_metrics = {}
+    main_metrics['epoch'] = []
+    savedir = os.getcwd() if savedir is None or savedir == '' else savedir
+
+    for epoch in range(start_epoch, num_epochs+1, 1):
+
+        # ----- run training and validation -----
+        
+        container_metrics = []
+        
+        if train_loader != None:
+            model, train_metrics = train_model(
+                model, train_loader, device, epoch, criterion, optimizer,
+                running_type = running_type, f1_average = f1_average,
+                validation_mode = False,
+                one_epoch_mode = True, 
+                metrics_list = metrics_list,
+                )
+            container_metrics.append(("train", train_metrics))
+
+        if test_loader != None:
+            model, test_metrics = train_model(
+                model, test_loader, device, epoch, criterion, optimizer,
+                running_type = running_type, f1_average = f1_average,
+                validation_mode = True,
+                one_epoch_mode = True, 
+                metrics_list = metrics_list,
+                )
+            container_metrics.append(("val.", test_metrics))
+        
+        # ----- collect metrics -----
+
+        main_metrics['epoch'].append(epoch)
+        
+        for phase, metrics in container_metrics:
+            for k, v in metrics.items():
+                new_k = f'{phase} {k}' # combine phase to dict's key
+                if new_k not in main_metrics:
+                    main_metrics[new_k] = v
+                else:
+                    main_metrics[new_k].extend(v)
+
+        stat_fname_path = os.path.join(savedir, f'stats_e{num_epochs}.json')
+        with open(stat_fname_path, 'w') as json_file:
+                json.dump(main_metrics, json_file, indent=4)
+
+        # ----- option to save checkpoints -----
+
+        if len(epoch_save_checkpoints)>0 and epoch in epoch_save_checkpoints:
+            checkpoint_path = os.path.join(savedir, 'checkpoints')
+            if not os.path.exists(checkpoint_path): os.makedirs(checkpoint_path) 
+            checkpoint = {
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'epoch': epoch,
+                }
+            checkpoint_save_path = os.path.join(checkpoint_path, 'checkpoint_e'+str(epoch)+'.pth')
+            torch.save(checkpoint, checkpoint_save_path)
+            checkpoint_stat_fname_path = os.path.join(checkpoint_path, 'checkpoint_e'+str(epoch)+'_stats.json')
+            with open(checkpoint_stat_fname_path, 'w') as json_file:
+                json.dump(main_metrics, json_file, indent=4)
+            print('Saved checkpoint at epoch', epoch)
+ 
+    # ----- option to save model -----
+
+    if save_model:
+        model_path = os.path.join(savedir, f'model_e{epoch}.pth')
+        torch.save(model, model_path)
+
+    return model, main_metrics
