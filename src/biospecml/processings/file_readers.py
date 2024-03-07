a7f2d1629bba364adfad91acc81c44eb47ceeaff
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2024-03-07 18:03:09 +0000

    convert_mat_oct

--- src/biospecml/processings/file_readers.py
+++ src/biospecml/processings/file_readers.py
@@ -44,3 +44,34 @@ def read_mat(filename):
         p = sp.reshape(sizex,h,w,order='C')
         return  w, h, p, wavenumber, sp
 
+
+def convert_mat_oct(sp, wn, tranpose_data:bool=False, key_name:str='ab', fname:str=None,
+                    return_array:bool=False
+                    ):
+    """
+    Convert spectral array from mat files to OCTAVVS friendly format by inserting
+    wavenumber value in the first postion of spectral data.
+
+    Args:
+    - sp (np.ndarray): is the spectral array that has (x, y) shape
+        - where x=features or wn, y=spectral data
+    - wn (np.ndarray): flatten wavenumber i.e: array([980, 982, ...])
+    - key_name (str): key name in the final converted mat file
+    - fname (str): filename to save files
+
+    """
+    if tranpose_data: sp = sp.T
+    
+    new_sp = np.empty((sp.shape[0], sp.shape[1]+1)) # +1 for wn
+
+    for i, w in enumerate(wn):
+        sp_i = sp[i]
+        sp_i = np.insert(sp_i, 0, w)
+        new_sp[i] = sp_i
+    
+    if isinstance(fname, str):
+        mat_arrays = {key_name: new_sp}
+        sio.savemat(fname, mat_arrays)
+    
+    if return_array:
+        return new_sp
