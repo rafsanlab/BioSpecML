505ac744bcaf696593fb2370084a6156f3aaa578
M. R. Rafsanjani <74598357+rafsanlab@users.noreply.github.com>
2024-03-11 19:38:10 +0000

    Update normalisations.py
    norm_peak

--- src/biospecml/processings/normalisations.py
+++ src/biospecml/processings/normalisations.py
@@ -1,15 +1,25 @@
 import numpy as np
 import pandas as pd
 
-def norm_peak(X, peak_range:range=(1680, 1620), mode:str='intensity', 
-              epsilon:float=1.4013e-45, wn=None):
+def norm_peak(X, peak_range:tuple=(1620, 1680), mode:str='intensity', 
+              epsilon:float=1.4013e-45, wn=None, verbose:bool=False):
     """
-    Applies normalisation on a dataframe based on wavenumber.
-    mode 'intensity' use the mean of the peak_range while
-    mode 'range' use the whole slice of peak_range.
-    if X is a dataframe; rows is the wn, and columns is the samples,
-    if X is a np.ndarray; use sp.
-    epsilon is a constant added to prevent division by zero.
+    Applies normalisation on a dataframe or np array based on wavenumber range.
+    
+    Args:
+    - X : matrix of spectral data;
+        - if X is a dataframe;
+            - rows is the wn, and columns is the samples,
+            - set wn as index
+            - peak_range must according to wn order
+        - if X is a np.ndarray, use sp and provide wn
+    - mode : mode of normalisation;
+        - mode 'intensity' use the mean of the peak_range
+        - mode 'range' use the whole slice of peak_range.
+    - epsilon : constant added to prevent division by zero.
+
+    Returns:
+    - X
 
     """
     if isinstance(X, pd.DataFrame):
@@ -19,7 +29,6 @@ def norm_peak(X, peak_range:range=(1680, 1620), mode:str='intensity',
             norm_values = X.loc[start:end].mean().mean()
         elif mode == 'range':
             norm_values = X.loc[start:end].mean()
-
         X = X / (norm_values + epsilon)
 
     elif isinstance(X, np.ndarray):
@@ -28,12 +37,14 @@ def norm_peak(X, peak_range:range=(1680, 1620), mode:str='intensity',
         start = peak_range[0]
         end = peak_range[1]
         indices = np.where((wn >= start) & (wn <= end))[0]
-        
         if mode == 'intensity':
             norm_values = np.mean(X[indices])
         elif mode == 'range':
             norm_values = np.mean(X[indices], axis=0)
-
         X = X / (norm_values + epsilon)
 
+    if verbose:
+        print('Wavenumber slice:', (wn[indices]))
+        print('Mean:', norm_values)
+    
     return X
